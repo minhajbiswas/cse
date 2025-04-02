32411986451667af625071bac867e8a6cc0bdf27
minhajbiswas <minhajuddin.biswas1234@gmail.com>
2025-04-02 22:36:42 +0530

    Add files via upload

---
+++ sub.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sub;
+
+    
+    printf("Enter the first integer: ");
+    scanf("%d", &num1);
+    
+    printf("Enter the second integer: ");
+    scanf("%d", &num2);
+
+   
+    sub= num1 + num2;
+
+    
+    printf("The sub of %d and %d is: %d\n", num1, num2, sum);
+
+    return 0;
+}
